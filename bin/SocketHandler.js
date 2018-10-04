597dbf035b761a45059f63479678b06880fcad62
IIINIII <ibrokhim.shokirov@gmail.com>
2018-10-04 17:58:59 +0300

    First changes

---
+++ bin/SocketHandler.js
@@ -0,0 +1,11 @@
+module.exports.init = function (io) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        socket.on('chat message', function (msg) {
+            console.log('message: ' + msg);
+        });
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+    });
+};
