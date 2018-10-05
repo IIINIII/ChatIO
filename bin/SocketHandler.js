e820d85e1049dfdc9e6041295465081cc3499c60
IIINIII <ibrokhim.shokirov@gmail.com>
2018-10-05 19:35:11 +0300

    Auth added

--- bin/SocketHandler.js
+++ bin/SocketHandler.js
@@ -1,11 +1,73 @@
 module.exports.init = function (io) {
+    var users = {};
+
     io.on('connection', function (socket) {
-        console.log('a user connected');
-        socket.on('chat message', function (msg) {
-            console.log('message: ' + msg);
+        var uniqueId = guidGenerator();
+
+        function checkAuth () {
+            if (users[uniqueId] == null || !users[uniqueId].authenticated) {
+                logout();
+            }
+        }
+
+        function logout () {
+            users[uniqueId].socket.disconnect();
+            delete users[uniqueId];
+        }
+
+        function guidGenerator () {
+            var S4 = function () {
+                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+            };
+            return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
+        }
+
+        console.log('[' + uniqueId + '] Connected! Waiting for auth!');
+
+        users[uniqueId] = {
+            id: uniqueId,
+            socket: socket,
+            authenticated: false,
+            authentication: null,
+            authAttempt: 0
+        };
+
+        // Check authentication after 3 seconds
+        setTimeout(function () {
+            checkAuth(uniqueId);
+        }, 5000);
+
+        socket.on('auth', function (data) {
+            if (data.username === 'admin' && data.password === '123e45') {
+                users[uniqueId].authenticated = true;
+                users[uniqueId].authentication = {
+                    username: data.username,
+                    password: data.password
+                };
+
+                console.log('[' + uniqueId + '] User Authenticated!');
+            } else {
+                users[uniqueId].authAttempt++;
+            }
         });
-        socket.on('disconnect', function(){
-            console.log('user disconnected');
+
+        socket.on('chat_msg', function (data) {
+            console.log('message to ' + data.to + ': ' + data.msg);
+            switch (data.to) {
+                case 'sys':
+                    // do something
+                    break;
+                case 'all':
+                    io.emit('chat_msg', { from: 'sys', msg: data.msg, to: data.to });
+                    break;
+                default:
+                    // do something else
+                    break;
+            }
+        });
+
+        socket.on('disconnect', function () {
+            console.log('[' + uniqueId + '] Disconnected!');
         });
     });
 };
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,39 @@
 $(function () {
-    var socket = io();
-    socket.emit('chat message', 'Hello World!');
+    var socket = null;
+
+    $('#formSendMessage').submit(function (event) {
+        $('#msg').val(function (index, value) {
+            if (socket) {
+                socket.emit('chat_msg', { msg: value, to: 'all' });
+            }
+
+            return '';
+        });
+
+        event.preventDefault();
+    });
+
+    $('#formLogin').submit(function () {
+        socket = io();
+
+        socket.emit('auth', { username: $('#username').val(), password: $('#password').val() });
+
+        socket.on('chat_msg', function (data) {
+            var msg = $(document.createElement('div'));
+            msg.text(data.msg);
+            msg.addClass('message');
+            msg = $(document.createElement('div')).append(msg);
+            msg.addClass('message-wrapper');
+
+            if (data.from === 'admin') {
+                msg.addClass('by-me');
+            } else {
+                msg.addClass('to-me');
+            }
+
+            $('#messages').append(msg);
+        });
+
+        event.preventDefault();
+    });
 });
